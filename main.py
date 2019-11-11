8cb4075efa5b798b17741ade2230ed67e239ed55
Radek Grebski <radoslaw.grebski@stepwise.pl>
2019-11-11 19:32:02 +0100

    Code cleanup, added comments

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from google.cloud import storage
 from google.cloud import translate_v2
 
-
-LANGUAGES_TO_TRANSLATE = {'PL', 'NO', 'ES', 'FR'}
+LANGUAGES_TRANSLATE_TO = {'PL', 'NO', 'ES', 'FR'}
 TMP_INPUT_FILE = '/tmp/input_file'
 TMP_OUTPUT_FILE = '/tmp/output_file'
 FILE_SUFFIX_TO_TRANSLATE = 'to_translate'
@@ -20,19 +19,21 @@ def translateFile(file_data, context):
         return
 
     bucket = storage_client.bucket(bucket_name)
+    # download file to /tmp (the only dir we have access to on google cloud functions)
     bucket.blob(file_name).download_to_filename(TMP_INPUT_FILE)
 
+    #read file content into input_str
     input_str = open(TMP_INPUT_FILE).read()
 
-    target_languages = LANGUAGES_TO_TRANSLATE
-
+    #write translated output into local /tmp
     with open(TMP_OUTPUT_FILE, 'w') as file:
         file.write("%s\n" % f'EN(original):\n{input_str}')
-        for language in target_languages:
+        for language in LANGUAGES_TRANSLATE_TO:
             translated = translate_client.translate(
                 input_str, target_language=language)
             file.write("%s\n" % f'{language}:\n{translated["translatedText"]}')
 
+    # upload local output from /tmp into bucket
     output_file_name = f'{file_name.rpartition(FILE_SUFFIX_TO_TRANSLATE)[0]}_translated'
     bucket.blob(output_file_name).upload_from_filename(TMP_OUTPUT_FILE)
 
